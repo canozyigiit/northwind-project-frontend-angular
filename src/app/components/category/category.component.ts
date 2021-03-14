6c8f79687763c64cf0943f9c7eceda75b4e4439f
Can Özyiğit <Muhammetcanozyigit@hotmail.com>
2021-03-14 17:23:16 +0300

    master

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/models/category';
+import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
   selector: 'app-category',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category.component.css']
 })
 export class CategoryComponent implements OnInit {
-
-  constructor() { }
+  categories:Category[]=[];
+  constructor(private categoryService:CategoryService) { }
 
   ngOnInit(): void {
+    this.getCategory();
   }
 
+  getCategory(){
+    this.categoryService.getCategories().subscribe(response=>{
+      this.categories=response.data
+    })
+  }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { ProductResponseModel } from 'src/app/models/productResponseModel';
 import { Observable } from 'rxjs';
+import { Product } from '../models/product';
+import { ListResponseModel } from '../models/listResponseModel';
 
 
 @Injectable({
@@ -11,7 +12,7 @@ export class ProductService {
   apiUrl = 'https://localhost:44314/api/products/getall'; 
   constructor(private httpClient: HttpClient) { }
 
-  getProducts():Observable<ProductResponseModel> {
-    return this.httpClient.get<ProductResponseModel>(this.apiUrl)
+  getProducts():Observable<ListResponseModel<Product>> {
+    return this.httpClient.get<ListResponseModel<Product>>(this.apiUrl)
   }
 }
