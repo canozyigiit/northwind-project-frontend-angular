bdb3d468a7f55cab1b8aa481eda44b77e841a9cf
Can Özyiğit <Muhammetcanozyigit@hotmail.com>
2021-03-14 19:49:12 +0300

    .

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -9,6 +9,7 @@ import { CategoryService } from 'src/app/services/category.service';
 })
 export class CategoryComponent implements OnInit {
   categories:Category[]=[];
+  currentCategory:Category;
   constructor(private categoryService:CategoryService) { }
 
   ngOnInit(): void {
@@ -17,7 +18,17 @@ export class CategoryComponent implements OnInit {
 
   getCategory(){
     this.categoryService.getCategories().subscribe(response=>{
-      this.categories=response.data
+      this.categories=response.data;
     })
+  }
+  setCurrentCategory(category:Category){
+    this.currentCategory=category;
+  }
+  getCurrentCategoryClass(category:Category){
+    if(category ==this.currentCategory){
+      return "list-group-item active"
+    }else{
+      return "list-group-item";
+    }
   }
 }
